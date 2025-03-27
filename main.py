f550acf0fc2c24fd02bfdd092ec9ff13cb756e86
Steven Cheung <stevencheung0690@outlook.com>
2025-03-27 15:49:35 +0000

    feat: added ability to get individual market data

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import json
+
+def get_individual_market_data(ticker: str, v_start: str, v_end: str, v_period: str, v_interval: str):
+    return yf.download(ticker, start=v_start, end=v_end, period=v_period, interval=v_interval)
