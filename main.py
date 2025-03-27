d020754c6953f117c7eaf43c547b669f8a74e3d0
Steven Cheung <stevencheung0690@outlook.com>
2025-03-27 17:02:39 +0000

    docs: added docstring

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import pandas as pd
 import csv
 
 def get_individual_market_data(ticker: str, v_start: str, v_end: str, v_period: str, v_interval: str):
+    """Fetches market data for a given stock ticker within a specified time range and saves it as a CSV file.
+
+    Args:
+        ticker (str): The stock ticker symbol.
+        v_start (str): The start date for the data retrieval in 'YYYY-MM-DD' format.
+        v_end (str): The end date for the data retrieval in 'YYYY-MM-DD' format.
+        v_period (str): The data period (e.g., "1d", "5d", "1mo", "3mo", "1y").
+        v_interval (str): The interval between data points (e.g., "1m", "5m", "1d", "1wk").
+
+    Returns:
+        _type_: A DataFrame containing the fetched market data.
+    """
     data = yf.download(ticker, start=v_start, end=v_end, period=v_period, interval=v_interval)
     with open("Market-Data/DataSets/"+v_start+"_"+v_end+"_"+ticker+".csv", "w") as f:
         for i in data.to_csv():
